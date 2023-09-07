57b95315f8ad51dc0e74800e83cd77c498dacc2e
moltam89 <moltam89@gmail.com>
2023-09-07 16:35:50 +0200

    Testing

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const { BigNumber, ethers, utils, Wallet } = require("ethers");
 
 const LI_FI_API_URL = "https://li.quest/v1";
 
+const provider = new ethers.providers.StaticJsonRpcProvider("https://polygon-rpc.com");
+const punkWallet = new ethers.Wallet(PUNK_PK, provider);
+
+
 const getQuote = async (fromChain, toChain, fromToken, toToken, fromAmount, fromAddress) => {
     const result = await axios.get('https://li.quest/v1/quote', {
         params: {
@@ -24,11 +28,11 @@ const getQuote = async (fromChain, toChain, fromToken, toToken, fromAmount, from
 
 
 
-const fromChain = 'DAI';
+const fromChain = 'POL';
 const fromToken = 'USDC';
-const toChain = 'POL';
+const toChain = 'DAI';
 const toToken = 'USDC';
-const fromAmount = '1000000';
+const fromAmount = '4000000';
 const fromAddress = PUNK_ADDR;
 
 
@@ -36,6 +40,11 @@ const fromAddress = PUNK_ADDR;
 const main = async () => {
 	const quote = await getQuote(fromChain, toChain, fromToken, toToken, fromAmount, fromAddress);
 	console.log("quote", quote);
+	console.log("quote.includedSteps.estimate", quote.includedSteps[0].estimate);
+	console.log("quote.includedSteps.toolDetails", quote.includedSteps[0].toolDetails);
+
+	//const tx = await punkWallet.sendTransaction(quote.transactionRequest);
+	//console.log("tx", tx);
 }
 
 main();
